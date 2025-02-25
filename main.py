1c499569cb9e635315eefb287bd47dc08f190445
Jack Bauerle <jackcbauerle@gmail.com>
2025-02-25 16:21:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from db import create_connection, execute_query
 
-mydb = create_connection("localhost", "root", "your_mysql_root_password", "my_sandbox_db")  # CHANGE PASSWORD HERE
+mydb = create_connection()
 
 if mydb:
     print("Successfully connected to the database!")

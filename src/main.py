eb0a229062335e572f4679c41f87f7e306ff0d48
Jack Bauerle <jackcbauerle@gmail.com>
2025-02-25 15:45:01 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,12 +3,7 @@ from db_utils import create_connection, execute_query
 mydb = create_connection("localhost", "root", "your_mysql_root_password", "my_sandbox_db")  # CHANGE PASSWORD HERE
 
 if mydb:
-    print("Connected to the database!")
-
-    # Example (uncomment to test):
-    # query = "SELECT VERSION()"
-    # results = execute_query(mydb, query)
-    # if results:
-    #     print(results)
-
+    print("Successfully connected to the database!")
     mydb.close()
+
+# TODO: run the setup_db script to initialize the database.

3f357569c302caee9f53786596bdd35359000c92
Jack Bauerle <jackcbauerle@gmail.com>
2025-02-25 17:10:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ mydb = create_connection()
 
 if mydb:
     print("Successfully connected to the database!")
+
+    print(execute_query("SELECT * FROM thing;"))
+    
     mydb.close()
 
 # TODO: run the setup_db script to initialize the database.

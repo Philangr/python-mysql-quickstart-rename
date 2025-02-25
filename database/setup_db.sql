cd932b153d64b038582868a2571de5f58139a107
Jack Bauerle <jackcbauerle@gmail.com>
2025-02-25 15:48:23 -0500

    Delete database/setup_db.sql

--- database/setup_db.sql
+++
@@ -1,8 +0,0 @@
--- Create your tables and insert initial data here.
-
-CREATE TABLE IF NOT EXISTS thing (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255)
-);
-
-INSERT INTO thing (name) VALUES ('thing1'), ('thing2');

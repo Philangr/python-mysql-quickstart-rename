7d0a5ddd3cfb4b0c04c95b1633d691dc8fd68d1a
Philangr <philipgross619@gmail.com>
2025-03-05 18:18:50 -0500

    Update setup_db.sql

--- setup_db.sql
+++ setup_db.sql
@@ -1,7 +1,7 @@
 -- Create your tables and insert initial data here.
-CREATE TABLE IF NOT EXISTS thing (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255)
-);
 
-INSERT INTO thing (name) VALUES ('OMG'), ('this is like totally data inside of a database!'), ('if you are reading this, it worked baby! you''re a full on software engineer now!');
+create table movies(
+title text,
+release_year int,
+genre text
+);

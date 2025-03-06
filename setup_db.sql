d1447baca6a0b6ff1ef421ed251668207c6b11e6
Philangr <philipgross619@gmail.com>
2025-03-06 18:27:38 -0500

    Update setup_db.sql

--- setup_db.sql
+++ setup_db.sql
@@ -1,11 +1,25 @@
 -- Create your tables and insert initial data here.
 
 create table movies(
+tmdb_id int primary key,
 title text,
-release_year int,
-genre text
+rating decimal(10,2)
 );
 
-alter table movies add column imdb_id varchar(10);
+create table people(
+tmdb_id int primary key,
+name text
+);
 
-alter table movies add primary key (imdb_id); 
+create table movies_people(
+movie_id int,
+people_id int,
+role_id int
+);
+
+create table `roles`(
+role_id int auto_increment primary key,
+`role` text
+);
+
+insert into `roles`(role) values("Actor"), ("Director")
